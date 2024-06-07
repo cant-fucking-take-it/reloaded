453b3f3711b54689095f828a464a77e8750f121a
Eina Safor Coma Bages <ecoma-ba@car4s1.42barcelona.com>
2024-06-07 13:58:55 +0200

    i ara?? x3

--- ex04/MAC.sh
+++ ex04/MAC.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-ifconfig -aL | sed -e '/ether/!d' -e 's/.*ether //' -e 's/ //'
+#!/bin/sh
+ip link | grep -Po "link/ether [0-9a-f]{2}(:[0-9a-f]{2}){5}" | grep -Po "[0-9a-f]{2}(:[0-9a-f]{2}){5}"
